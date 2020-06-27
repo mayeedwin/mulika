11e33766a185b71af62b0e773fa1ee3ca553756b
Maye Edwin <mayedwine1@gmail.com>
2020-06-27 11:27:00 +0300

    Clean

--- src/index.js
+++ src/index.js
@@ -8,13 +8,13 @@ const mulika = {
     const type = Object.prototype.toString.call(item.value).slice(8, -1);
     item_col = `${item.type.toLowerCase()}`.yellow;
     if (type.toLowerCase() === item.type.toLowerCase()) {
-      result = `✔️  Passed type check of typeof ${item_col}`.green;
+      result = ` ✔️  Passed type check of typeof ${item_col}`.green;
       console.log(result);
     } else {
-      const err = new Error(`❌ Failed Type Check`);
+      const err = new Error(` ❌ Failed Type Check`);
       const stack = err.stack.split("\n");
       const line_path = stack[2].split("/");
-      result = `❌ Expected typeof ${item_col}, not ${type.toLowerCase()}
+      result = ` ❌ Expected typeof ${item_col}, not ${type.toLowerCase()}
 ${line_path}`.red;
       console.log(result);
     }
@@ -23,14 +23,16 @@ ${line_path}`.red;
   expect(message, callback, check) {
     let callback_col;
     if (callback === check) {
-      console.log(`✔️ Expected ${message}`.green);
+      console.log(` ✔️  Expected ${message}`.green);
     } else {
       const err = new Error(`❌ Test Failed`);
       const stack = err.stack.split("\n");
       const line_path = stack[2].split("/");
       callback_col = `${callback}`.yellow;
-      console.log(`❌ Expected ${message} not ${callback_col}
-${line_path}`.red);
+      console.log(
+        ` ❌ Expected ${message} not ${callback_col}
+${line_path}`.red
+      );
     }
   },
 };
