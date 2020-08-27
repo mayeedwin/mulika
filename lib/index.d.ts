fd96e0c3f847a62f9ce9a139441e9db7791f6339
Maye Edwin <mayedwine1@gmail.com>
2020-08-27 10:58:54 +0300

    del prod

--- lib/index.d.ts
+++
@@ -1,8 +0,0 @@
-declare const mulika: {
-    typeCheck(item: {
-        value: any;
-        type: string;
-    }): void;
-    expect(message: string, value: any, assertion: any): void;
-};
-export default mulika;
